fead26ccd3a2fcac8cbbbe01f424b9d543e1c0da
vittoaf <80021734+vittoaf@users.noreply.github.com>
2022-09-12 18:59:54 -0500

    Nueva tabla comercial

--- Trabajo/Rimac/Funnel Ventas/CalendarioComercial.sql
+++ Trabajo/Rimac/Funnel Ventas/CalendarioComercial.sql
@@ -1,5 +1,6 @@
 CREATE  OR REPLACE TABLE `rs-nprd-dlk-dt-stg-mica-4de1.delivery_canales.NuevoCalendario_Vitto`
 AS
+With Calendario as (
 Select '2022-01-27' FecCierre,'2022-01-28' FecSuscripcion,'2022-01-31'FecEmision
 UNION ALL
 Select '2022-02-24','2022-02-25','2022-02-28'
@@ -22,4 +23,8 @@ Select '2022-10-27','2022-10-28','2022-10-31'
 UNION ALL
 Select '2022-11-28','2022-11-29','2022-11-30'
 UNION ALL
-Select '2022-12-28','2022-12-29','2022-12-30'
+Select '2022-12-28','2022-12-29','2022-12-30')
+
+Select cast(FecCierre as TIMESTAMP) FecCierre,
+cast(FecSuscripcion as TIMESTAMP) FecSuscripcion,
+cast(FecEmision as TIMESTAMP) FecEmision FROM Calendario
